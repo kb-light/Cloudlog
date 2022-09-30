4cf273d372f40aefa0ec2d5c20f75ca80cc6c651
phl0 <github@florian-wolters.de>
2022-09-30 07:57:21 +0200

    Fix indentation (tab vs. space)
    Signed-off-by: phl0 <github@florian-wolters.de>

--- application/migrations/100_update_cat_table.php
+++ application/migrations/100_update_cat_table.php
@@ -11,23 +11,23 @@ class Migration_update_cat_table extends CI_Migration {
     public function up()
     {
         if ($this->db->table_exists('cat')) {
-			$this->dbforge->drop_column('cat', 'uplink_freq');
-			$this->dbforge->drop_column('cat', 'uplink_mode');
-			$this->db->query("ALTER TABLE cat RENAME COLUMN downlink_mode TO mode_rx");
-			$this->db->query("ALTER TABLE cat RENAME COLUMN downlink_freq TO frequency_rx");
-		}
+            $this->dbforge->drop_column('cat', 'uplink_freq');
+            $this->dbforge->drop_column('cat', 'uplink_mode');
+            $this->db->query("ALTER TABLE cat RENAME COLUMN downlink_mode TO mode_rx");
+            $this->db->query("ALTER TABLE cat RENAME COLUMN downlink_freq TO frequency_rx");
+        }
     }
 
     public function down()
-	{
+    {
         if ($this->db->table_exists('cat')) {
-			$this->db->query("ALTER TABLE cat RENAME COLUMN mode_rx TO downlink_mode");
-			$this->db->query("ALTER TABLE cat RENAME COLUMN frequency_rx TO downlink_freq");
+            $this->db->query("ALTER TABLE cat RENAME COLUMN mode_rx TO downlink_mode");
+            $this->db->query("ALTER TABLE cat RENAME COLUMN frequency_rx TO downlink_freq");
 
             if (!$this->db->field_exists('uplink_freq', 'cat')) {
                 $fields = array(
-					'uplink_freq bigint(13) DEFAULT NULL AFTER `downlink_freq`',
-					'uplink_mode varchar(255) DEFAULT NULL AFTER `downlink_mode`',
+                    'uplink_freq bigint(13) DEFAULT NULL AFTER `downlink_freq`',
+                    'uplink_mode varchar(255) DEFAULT NULL AFTER `downlink_mode`',
                 );
                 $this->dbforge->add_column('cat', $fields);
             }
